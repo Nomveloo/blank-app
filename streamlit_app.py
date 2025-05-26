0bc19b6cbbb8f6406fa006c4467662af283ead48
Nomvelo Tembe <166735518+Nomveloo@users.noreply.github.com>
2025-05-26 21:34:52 +0000

    fix bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.header("Your new project")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st.selectbox("Your new project”, (“First)”, (“Second”)")
+st.selectbox("Your new project", ("First", "Second"))
 st.button("Generate")
 
 
