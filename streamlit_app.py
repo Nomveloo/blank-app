9e6fde0a7f7ee843a6f5d7e0df649d78c30f403d
Nomvelo Tembe <166735518+Nomveloo@users.noreply.github.com>
2025-05-26 21:21:08 +0000

    Updated changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new Streamlit app")
+st.title("🎈 Testing Project")
+st.header("Your new project")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
